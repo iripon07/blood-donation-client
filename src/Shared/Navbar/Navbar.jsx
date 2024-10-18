3f9ad2a8dd1259e76995f10ce75f7349a8cc015c
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-10-18 13:00:24 +0600

    Navbar addeed

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className="container mx-auto ">
+      <div
+        style={{ border: "1px solid red" }}
+        className="flex justify-between items-center h-[100px]"
+      >
+        <div >
+          <img
+            src={logo}
+            className="w-[150px] h-[50px]"
+            alt="Logo of the site"
+          />
+        </div>
+        <div className="text-xl text-secondary space-x-2">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/campaign">Campaign</NavLink>
+          <NavLink to="/blogs">Blogs</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/about">About Us</NavLink>
+        </div>
+        <div className="text-xl text-secondary space-x-2">
+          <Link>Login</Link>
+          <Link>Sign Up</Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -4,9 +4,8 @@ import "swiper/css/pagination";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Banner1 from "../../assets/Banner1.jpg";
-import Banner2 from "../../assets/Banner2.jpg";
+import Banner2 from "../../assets/Banner2.png";
 import Banner3 from "../../assets/Banner3.jpg";
-import Styles from "./Banner.module.css"
 import "./banner.css"
 
 
@@ -30,12 +29,12 @@ const Banner = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <div>
-            <img className="w-full md:h-[600px] h-full" src={Banner1} alt="" />
+          <div >
+            <img className="w-full md:h-[600px]" src={Banner1} alt="" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div>
+          <div >
             <img className="w-full md:h-[600px] h-full" src={Banner2} alt="" />
           </div>
         </SwiperSlide>
