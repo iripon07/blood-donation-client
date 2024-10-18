e2ebe1f8765a1bbab298413bf4572c6f424f5477
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-10-19 00:43:56 +0600

    Navbar added

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -5,22 +5,71 @@ import logo from "../../assets/logo.png";
 const Navbar = () => {
   return (
     <div className="container mx-auto">
-      <div
-        className="flex justify-between items-center"
-      >
-        <div >
+      <div className="flex justify-between items-center">
+        <div>
           <img
             src={logo}
-            className="w-[100px] h-[100px]"
+            className="w-[120px] h-[100px]"
             alt="Logo of the site"
           />
         </div>
         <div className="text-xl text-secondary space-x-4">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/campaign">Campaign</NavLink>
-          <NavLink to="/blogs">Blogs</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-          <NavLink to="/about">About Us</NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "text-primary text-[21px] underline font-semibold transition duration-300"
+                : "hover:text-primary font-semibold transition duration-300"
+            }
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "text-primary text-[21px]  font-semibold transition duration-300 "
+                : "hover:text-primary  font-semibold animate-slideInLeft"
+            }
+            to="/campaign"
+          >
+            Campaign
+          </NavLink>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "text-green"
+                : isActive
+                ? "text-primary"
+                : "hover:text-primary"
+            }
+            to="/blogs"
+          >
+            Blogs
+          </NavLink>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "text-green"
+                : isActive
+                ? "text-primary"
+                : "hover:text-primary"
+            }
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "text-green"
+                : isActive
+                ? "text-primary"
+                : "hover:text-primary"
+            }
+            to="/about"
+          >
+            About Us
+          </NavLink>
         </div>
         <div className="text-xl text-secondary space-x-2">
           <Link>Login</Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,19 @@ export default {
     extend: {
       colors: {
         primary: '#991b1b',
-        secondary:"#ef4444",
+        secondary: "#ef4444",
         white: '#FFFFFF',
       },
-},
+      keyframes: {
+        slideInLeft: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        slideInLeft: 'slideInLeft 1s ease-out',
+      },
+    },
   },
   plugins: [
   ],
