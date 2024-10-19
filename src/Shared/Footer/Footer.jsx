0a31ce6a775526ca83cddb60e1f9f69421ca86bc
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-10-19 13:49:16 +0600

    Sub footer added

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -2,25 +2,27 @@
 
 const Footer = () => {
   return (
-   <footer className="w-full">
-    <div className="flex text-white font-bold">
-      <div className="bg-black w-1/2">
-        <h1>Lorem, ipsum.</h1>
-        <h1>Lorem, ipsum.</h1>
-        <h1>Lorem, ipsum.</h1>
-        <h1>Lorem, ipsum.</h1>
-
+    <footer className="w-full">
+      <div className="flex text-white font-bold">
+        <div className="bg-black w-1/2">
+          <h1>Lorem, ipsum.</h1>
+          <h1>Lorem, ipsum.</h1>
+          <h1>Lorem, ipsum.</h1>
+          <h1>Lorem, ipsum.</h1>
+        </div>
+        <div className="bg-primary w-1/2">
+          <h2>Lorem, ipsum dolor.</h2>
+          <h2>Lorem, ipsum dolor.</h2>
+          <h2>Lorem, ipsum dolor.</h2>
+          <h2>Lorem, ipsum dolor.</h2>
+        </div>
       </div>
-      <div className="bg-primary w-1/2">
-        <h2>Lorem, ipsum dolor.</h2>
-        <h2>Lorem, ipsum dolor.</h2>
-        <h2>Lorem, ipsum dolor.</h2>
-        <h2>Lorem, ipsum dolor.</h2>
-
+      <div className="bg-black">
+        <div className=" text-white font-semibold text-base container mx-auto py-6 text-center">
+          <h6>©2024 LifeBank. All rights reserved</h6>
+        </div>
       </div>
-
-    </div>
-   </footer>
+    </footer>
   );
 };
 
